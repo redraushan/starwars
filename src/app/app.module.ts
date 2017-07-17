84adacb56a3a2c606cb388b01724888cda813c9f
Raushan Sharma <raushan.sharma@sparkwork.io>
2017-07-17 17:23:05 +0530

    Project added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,52 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {AppComponent} from './app.component';
+import {LoginComponent} from './login/login.component';
+import {HomeComponent} from './home/home.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MdGridListModule, MdCardModule, MdButtonModule, MdInputModule, MdSnackBarModule} from '@angular/material';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import 'hammerjs';
+import {HttpModule} from '@angular/http';
+import {PlanetComponent} from './planet/planet.component';
+import {AuthGuard} from './guards/auth.guard';
+const appRoutes: Routes = [
+  {
+    path: 'home', canActivate: [AuthGuard],
+    component: HomeComponent
+  },
+  {
+    path: '', redirectTo: 'login', pathMatch: 'full'
+  },
+  {
+    path: 'login', component: LoginComponent
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    PlanetComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    BrowserAnimationsModule,
+    MdGridListModule,
+    MdButtonModule,
+    MdCardModule,
+    FormsModule,
+    MdSnackBarModule,
+    ReactiveFormsModule,
+    MdInputModule,
+    RouterModule.forRoot(appRoutes)
+
   ],
-  providers: [],
+  providers: [AuthGuard],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  endpoint: 'https://swapi.co/api/'
+
 };
